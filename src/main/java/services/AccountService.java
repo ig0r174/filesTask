a8abbf7583d0f9910de45e95a9593e1cf7419005
igorpetrov <hstllbg@icloud.com>
2021-10-19 19:28:02 +0500

    Module 6

--- src/main/java/services/AccountService.java
+++ src/main/java/services/AccountService.java
@@ -1,34 +1,67 @@
 package services;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AccountService {
-    private final Map<String, UserProfile> loginToProfile;
-    private final Map<String, UserProfile> sessionIdToProfile;
+    private static final Map<String, UserProfile> loginToProfile;
+    private static final Map<String, UserProfile> sessionIdToProfile;
+    private static final DBService dbService;
 
-    public AccountService() {
+    static {
         loginToProfile = new HashMap<>();
         sessionIdToProfile = new HashMap<>();
+        dbService = new DBService();
+        initializeDBService();
     }
 
-    public void addNewUser(UserProfile userProfile) {
+    private static void initializeDBService() {
+        try{
+            ResultSet rs = dbService.getStatement().executeQuery("SELECT `Login`, `Pass`, `Email` FROM `Users`");
+            while (rs.next()){
+
+                loginToProfile.put(
+                        rs.getString(1),
+                        new UserProfile(rs.getString(1), rs.getString(2), rs.getString(3))
+                );
+                String login = rs.getString(1);
+                System.out.println("Login: " + login);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createNewUser(String login, String email, String pass){
+        try{
+            String sql = String.format("INSERT INTO `Users` (`Login`, `Email`, `Pass`) values ('%s', '%s', '%s');", login, email, pass);
+            dbService.getStatement().executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addNewUser(UserProfile userProfile) {
+        createNewUser(userProfile.getLogin(), userProfile.getEmail(), userProfile.getPass());
         loginToProfile.put(userProfile.getLogin(), userProfile);
     }
 
-    public UserProfile getUserByLogin(String login) {
+    public static UserProfile getUserByLogin(String login) {
         return loginToProfile.getOrDefault(login, null);
     }
 
-    public UserProfile getUserBySessionId(String sessionId) {
+    public static UserProfile getUserBySessionId(String sessionId) {
         return sessionIdToProfile.getOrDefault(sessionId, null);
     }
 
-    public void addSession(String sessionId, UserProfile userProfile) {
+    public static void addSession(String sessionId, UserProfile userProfile) {
         sessionIdToProfile.put(sessionId, userProfile);
     }
 
-    public void deleteSession(String sessionId) {
+    public static void deleteSession(String sessionId) {
         sessionIdToProfile.remove(sessionId);
     }
 }
